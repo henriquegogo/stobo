3aa85ad154f3fd7427eafb786f49b27e01ec84b7
Henrique Gogó <henriquegogo@gmail.com>
2015-06-23 00:47:27 -0300

    Updating rockspec file with dependencies and using yahoo as source of data

--- stobo-0.1-1.rockspec
+++ stobo-0.1-1.rockspec
@@ -1,9 +1,10 @@
 package = 'stobo'
 version = '0.1-1'
-source = { url = '*' }
+source = { url = 'Stobo Repository' }
 dependencies = {
   'lua ~> 5.1',
   'inspect', 'luasocket', 'luazip',
-  'lua-gnuplot', 'lgi', 'ansicolors'
+  'lua-gnuplot', 'lgi', 'ansicolors',
+  'luasec', 'lua-cjson'
 }
 build = { type = 'make' }
---
+++ yahoo-stobo.lua
@@ -0,0 +1,97 @@
+#!./bin/lua
+
+local inspect = require 'inspect'
+local http = require 'socket.http'
+local https = require 'ssl.https'
+local cjson = require 'cjson'
+
+local api_url = 'https://finance-yql.media.yahoo.com/v7/finance/chart/%s?period1=%s&period2=%s&interval=%s'
+
+-- Quote
+Quote = {} do
+  local numberOrNil = function(number)
+    if type(number) == 'number' then
+      return number
+    else
+      return 0
+    end
+  end
+  
+  local floatToInt = function(number)
+    if type(number) == 'number' then
+      return math.floor(number)
+    end
+  end
+
+  function Quote.new(timestamp, open, low, high, close, volume)
+    local self = setmetatable({}, { __index = Quote })
+
+    local datetable = os.date('*t', timestamp)
+
+    self.datetime = ('%04d%02d%02d%02d%02d'):format(datetable.year,
+                    datetable.month, datetable.day,
+                    datetable.hour, datetable.min)
+    
+    self.open = numberOrNil(open)
+    self.low = numberOrNil(low)
+    self.high = numberOrNil(high)
+    self.close = numberOrNil(close)
+    self.volume = floatToInt(numberOrNil(volume))
+    
+    return self
+  end
+end
+
+-- Stock
+Stock = {} do
+  function Stock.new(symbol)
+    local self = setmetatable({}, { __index = Stock })
+    
+    self.symbol = symbol
+    self.quotes = {}
+
+    return self
+  end
+
+  function Stock:get(options)
+    local period_start = options and options.period_start or
+                         os.time{year=2015, month=6, day=22, hour=10}
+    local period_stop  = options and options.period_stop or
+                         os.time()
+    local interval     = options and options.interval or
+                         '1m'
+
+    local url = (api_url):format(self.symbol, period_start, period_stop, interval)
+    local json = https.request(url)
+    local table = cjson.decode(json)
+    local result = table.chart.result[1]
+    local data = result.indicators.quote[1]
+
+    for i,timestamp in ipairs(result.timestamp) do
+      local quote = Quote.new(timestamp,
+                          data.open[i],
+                          data.low[i],
+                          data.high[i],
+                          data.close[i],
+                          data.volume[i])
+
+      self.quotes[quote.datetime] = quote
+    end
+
+    return self
+  end
+end
+
+-- Main
+do
+  local stock = Stock.new('TIMP3.SA'):get{ interval = '2m' }
+
+  for i,quote in pairs(stock.quotes) do
+    local result = ('%s - O: %.2f H: %.2f L: %.2f C: %.2f V: %s'):format(
+                     quote.datetime, quote.open,
+                     quote.high, quote.low,
+                     quote.close, quote.volume)
+
+    print(result)
+  end
+end
