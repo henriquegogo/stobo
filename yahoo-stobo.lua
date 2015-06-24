0d74f833ad89a609b19b84d79e497b11b3ccbff9
Henrique Gogó <henriquegogo@gmail.com>
2015-06-24 00:46:14 -0300

    Show colors and formated output

--- yahoo-stobo.lua
+++ yahoo-stobo.lua
@@ -4,6 +4,7 @@ local inspect = require 'inspect'
 local http = require 'socket.http'
 local https = require 'ssl.https'
 local cjson = require 'cjson'
+local colors = require 'ansicolors'
 
 local api_url = 'https://finance-yql.media.yahoo.com/v7/finance/chart/%s?period1=%s&period2=%s&interval=%s'
 
@@ -54,12 +55,21 @@ Stock = {} do
   end
 
   function Stock:get(options)
-    local period_start = options and options.period_start or
-                         os.time{year=2015, month=6, day=22, hour=10}
-    local period_stop  = options and options.period_stop or
-                         os.time()
-    local interval     = options and options.interval or
-                         '1m'
+    local period_start = options and options.period_start and os.time
+                            {year = tonumber(options.period_start:sub(1,4)),
+                            month = tonumber(options.period_start:sub(5,6)),
+                            day = tonumber(options.period_start:sub(7,8)),
+                            hour = 0}
+                            or os.time()
+
+    local period_stop  = options and options.period_stop and os.time
+                            {year = tonumber(options.period_stop:sub(1,4)),
+                            month = tonumber(options.period_stop:sub(5,6)),
+                            day = tonumber(options.period_stop:sub(7,8)),
+                            hour = 23}
+                            or os.time()
+
+    local interval     = options and options.interval or '15m'
 
     local url = (api_url):format(self.symbol, period_start, period_stop, interval)
     local json = https.request(url)
@@ -84,15 +94,28 @@ end
 
 -- Main
 do
-  local stock = Stock.new('TIMP3.SA'):get{ interval = '2m' }
+  local stock = Stock.new('TIMP3.SA'):get{ interval = '2m', period_start = '20150619' }
   
   print(stock.symbol)
   for i,quote in ipairs(stock.quotes) do
-    local result = ('%s - O: %.2f H: %.2f L: %.2f C: %.2f V: %s'):format(
-                     quote.datetime, quote.open,
-                     quote.high, quote.low,
-                     quote.close, quote.volume)
+    local result = ('%s - O: %5s H: %5s L: %5s C: %5s V: %7s'):format(
+                     quote.datetime,
+                     ('%.2f'):format(quote.open),
+                     ('%.2f'):format(quote.high),
+                     ('%.2f'):format(quote.low),
+                     ('%.2f'):format(quote.close),
+                     quote.volume)
 
-    print(result)
+    local last_quote = stock.quotes[i-1]
+
+    local close_color = last_quote and quote.close > last_quote.close and '%{green}'
+               or last_quote and quote.close < last_quote.close and '%{red}'
+               or '%{}'
+               
+    local candle_color = quote.close > quote.open and '%{greenbg}'
+                      or quote.close < quote.open and '%{redbg}'
+                      or '%{}'
+
+    print( colors(candle_color..result) )
   end
 end
