d7e186e77dee6b817c607030716df3f81fc3e8a8
Henrique Gogó <henriquegogo@gmail.com>
2016-01-25 00:39:42 -0300

    Parsing the result with cJSON

--- src/main.c
+++ src/main.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <curl/curl.h>
+#include <cJSON/cJSON.h>
 
 char url[] = "https://finance-yql.media.yahoo.com/v7/finance/chart/PETR4.SA";
 char *result;
@@ -27,10 +28,17 @@ int main(int argc, char const *argv[]) {
   curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, request_callback);
   curl_easy_perform(curl);
 
-  printf("%s\n", result);
+  cJSON *json_object = cJSON_Parse(result);
+  json_object = cJSON_GetObjectItem(json_object, "chart");
+  json_object = cJSON_GetObjectItem(json_object, "result");
+  json_object = cJSON_GetArrayItem(json_object, 0);
+  json_object = cJSON_GetObjectItem(json_object, "meta");
+  json_object = cJSON_GetObjectItem(json_object, "symbol");
+  printf("%s\n", json_object->valuestring);
 
   free(result);
   curl_easy_cleanup(curl);
+  cJSON_Delete(json_object);
 
   return 0;
 }
