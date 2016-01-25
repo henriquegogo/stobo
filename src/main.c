37390190f2a4e9c60d8aee700c8bcf56d347fb7e
Henrique Gogó <henriquegogo@gmail.com>
2016-01-25 15:50:21 -0300

    Getting result size programaticaly

--- src/main.c
+++ src/main.c
@@ -2,15 +2,13 @@
 #include <stdlib.h>
 #include <string.h>
 #include <curl/curl.h>
-#include <cJSON/cJSON.h>
+//#include <cJSON/cJSON.h>
 
-char url[] = "https://finance-yql.media.yahoo.com/v7/finance/chart/PETR4.SA";
 char *result;
-size_t result_size;
-CURL *curl;
 
 size_t request_callback(char *content, size_t size, size_t nmemb, void *userdata) {
   size_t length = size * nmemb;
+  size_t result_size = result ? strlen(result) * sizeof(char) : 0;
 
   result = realloc(result, result_size + length + 1);
   memcpy(&result[result_size], content, length);
@@ -21,6 +19,9 @@ size_t request_callback(char *content, size_t size, size_t nmemb, void *userdata
 }
 
 int main(int argc, char const *argv[]) {
+  char url[] = "https://finance-yql.media.yahoo.com/v7/finance/chart/PETR4.SA";
+  CURL *curl;
+
   curl = curl_easy_init();
 
   curl_easy_setopt(curl, CURLOPT_HTTPGET, 1);
@@ -28,6 +29,9 @@ int main(int argc, char const *argv[]) {
   curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, request_callback);
   curl_easy_perform(curl);
 
+  printf("%s\n", result);
+
+  /*
   cJSON *json_object = cJSON_Parse(result);
   json_object = cJSON_GetObjectItem(json_object, "chart");
   json_object = cJSON_GetObjectItem(json_object, "result");
@@ -36,9 +40,11 @@ int main(int argc, char const *argv[]) {
   json_object = cJSON_GetObjectItem(json_object, "symbol");
   printf("%s\n", json_object->valuestring);
 
+  cJSON_Delete(json_object);
+  */
+
   free(result);
   curl_easy_cleanup(curl);
-  cJSON_Delete(json_object);
 
   return 0;
 }
